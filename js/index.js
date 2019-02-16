0769991495122e6cd083a6202239440d12a8ebe3
Ronny Alvarado <rsalvarado777@gmail.com>
2019-02-16 12:24:11 -0600

    Working on expanded navigation - problems regarding the viewport

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+
+class Dropdown {
+    constructor(element) {
+      this.element = element;
+      this.button = this.element.querySelector('.dropdown-button');
+      this.content = this.element.querySelector('.dropdown-content');
+      this.button.addEventListener('click', () =>  {
+        this.toggleContent();
+      });
+    }
+    toggleContent() {
+      this.content.classList.toggle('dropdown-hidden');
+    }
+  }
+  let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
